5dca189d50457d095f17433740c06ecb12b09c98
sr_boro <11045297+oscarda@users.noreply.github.com>
2022-04-25 13:28:11 +0200

    Update blackjack.py
    corrección de error menor

--- act1/blackjack.py
+++ act1/blackjack.py
@@ -4,62 +4,63 @@ import random
 
 
 def sacar_carta():
-	valor_carta= random.randrange(1,12)
-	print ("Su carta es ", valor_carta)
-	return valor_carta
+    valor_carta= random.randrange(1,12)
+    print ("Su carta es ", valor_carta)
+    return valor_carta
 
 ##########################################
 
-def comprueba_fin_juego(puntos):	
-	if puntos==21:
-		print ("Enhorabuena has llegado a 21!!!")
-		seguir_jugando = False
-	
-	elif puntos>21:
-		print ("Lo siento, has superado 21!!!")
-		seguir_jugando = False
-	
-	else:
-		respuesta = input("¿Deseas otra carta? (S/n):  ")
-		if respuesta == "n":
-			print ("Te plantas con ", puntos, ".")
-			print ("Te quedas a ", 21-puntos, "de llegar a 21.")
-			seguir_jugando=False
-			
-	#fin if-elif-else
-	
-	return seguir_jugando
+def comprueba_fin_juego(puntos):
+    seguir_jugando = True
+    if puntos==21:
+        print ("Enhorabuena has llegado a 21!!!")
+        seguir_jugando = False
+    
+    elif puntos>21:
+        print ("Lo siento, has superado 21!!!")
+        seguir_jugando = False
+    
+    else:
+        respuesta = input("¿Deseas otra carta? (S/n):  ")
+        if respuesta == "n":
+            print ("Te plantas con ", puntos, ".")
+            print ("Te quedas a ", 21-puntos, "de llegar a 21.")
+            seguir_jugando=False
+            
+    #fin if-elif-else
+    
+    return seguir_jugando
 
 ##########################################
-	
-	
-# función principal del juego	
+    
+    
+# función principal del juego    
 def algoritmo_principal():
-	
-	print ("Bienvenido usuario al juego de Sumar 21")
-	
-	while True:	
+    
+    print ("Bienvenido usuario al juego de Sumar 21")
+    
+    while True:    
 
-		print("\n\nIniciamos una nueva partida")
-		
-		puntos=0 				
-		seguir_jugando = True	
-		respuesta = "s"			
-		
-		while seguir_jugando:
-			puntos += sacar_carta()
-			print ("\ttienes ", puntos, "puntos...")
-			seguir_jugando=comprueba_fin_juego(puntos)
+        print("\n\nIniciamos una nueva partida")
+        
+        puntos=0                 
+        seguir_jugando = True    
+        respuesta = "s"            
+        
+        while seguir_jugando:
+            puntos += sacar_carta()
+            print ("\ttienes ", puntos, "puntos...")
+            seguir_jugando=comprueba_fin_juego(puntos)
 
-		#Fin de la partida, ¿jugamos otra?
-		if "n" == input("¿Desea jugar otra partida? (S/n): "):
-			break;
-	
-	print ("Gracias por jugar con nosotros. Vuelva pronto!!")
+        #Fin de la partida, ¿jugamos otra?
+        if "n" == input("¿Desea jugar otra partida? (S/n): "):
+            break;
+    
+    print ("Gracias por jugar con nosotros. Vuelva pronto!!")
 
-##########################################	
-	
-	
+##########################################    
+    
+    
 #Aquí inicia la ejecución del programa
 algoritmo_principal()
 
